3eb533c0b8aaf0528bfccc0b0d5eb1adf889b4fb
ifflagged <ifflagged@users.noreply.github.com>
2024-11-25 08:26:58 +0000

    Converted modules at 2024-11-25 16:26:58 (UTC+8)

--- Modules/JS/zwf234/qxrules/monkey.js
+++ Modules/JS/zwf234/qxrules/monkey.js
@@ -1,14 +1,7 @@
-var obj = JSON.parse($response.body);
-
-obj.data.purchased=
-{
-      "product_list": [
-      ],
-      "is_active": true,
-      "time_expire": 0,
-      "pay_inapp": true,
-      "is_free": false
-    };
-$done({body: JSON.stringify(obj)});
-
-// Descriptionsp
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<h1>502 Bad Gateway</h1>
+<p>The proxy server received an invalid response from an upstream server.<hr/>Powered by Tengine</body>
+</html>
