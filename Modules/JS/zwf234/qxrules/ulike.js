988dba1605128036233920cefd77cad96bbe60a7
ifflagged <ifflagged@users.noreply.github.com>
2024-11-26 03:33:40 +0000

    Converted modules at 2024-11-26 11:33:40 (UTC+8)

--- Modules/JS/zwf234/qxrules/ulike.js
+++ Modules/JS/zwf234/qxrules/ulike.js
@@ -1,16 +1,7 @@
-var obj = JSON.parse($response.body);
-
-obj= {
-  "data": {
-    "flag": true,
-    "start_time": 1572760027,
-    "end_time": 4097368706
-  },
-  "systime": "",
-  "errmsg": "Success",
-  "ret": "0"
-};
-
-$done({body: JSON.stringify(obj)});
-
-//LK
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<h1>502 Bad Gateway</h1>
+<p>The proxy server received an invalid response from an upstream server.<hr/>Powered by Tengine</body>
+</html>
