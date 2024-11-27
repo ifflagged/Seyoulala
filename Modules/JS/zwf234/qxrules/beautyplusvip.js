09a85b4e575064318c9a1e13736fe5272100b06d
ifflagged <ifflagged@users.noreply.github.com>
2024-11-27 06:28:36 +0000

    Converted modules at 2024-11-27 14:28:36 (UTC+8)

--- Modules/JS/zwf234/qxrules/beautyplusvip.js
+++ Modules/JS/zwf234/qxrules/beautyplusvip.js
@@ -1,11 +1,7 @@
-var obj = JSON.parse($response.body);
-
-
-obj=  {
-  "status": 0,
-  "expires_date": "9999-06-06 19:57:41"
-};
-$done({body: JSON.stringify(obj)});
-
-
-// Mô tả
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<h1>502 Bad Gateway</h1>
+<p>The proxy server received an invalid response from an upstream server.<hr/>Powered by Tengine</body>
+</html>
--- Modules/JS/zwf234/qxrules/mondly.vip.js
+++ Modules/JS/zwf234/qxrules/mondly.vip.js
@@ -1,13 +1,7 @@
-var obj = JSON.parse($response.body);
-
-obj["pucharses"]= [10];
-obj["subscriptions"]=  {
-  "10": "2099-09-09 05:05:05"
-};
-obj["subscription"]=  {
-  "10": "2099-09-09 05:05:05"
-};
-obj["parsed_transactions"]= [
-  "540000370283138"
-];
-$done({body: JSON.stringify(obj)});
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<h1>502 Bad Gateway</h1>
+<p>The proxy server received an invalid response from an upstream server.<hr/>Powered by Tengine</body>
+</html>
