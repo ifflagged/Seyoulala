d4df5f6d812609b6ca3212933c34bf5b8b613308
ifflagged <ifflagged@users.noreply.github.com>
2024-11-28 14:19:34 +0000

    Converted modules at 2024-11-28 22:19:33 (UTC+8)

--- Modules/JS/zwf234/qxrules/itranslate.js
+++ Modules/JS/zwf234/qxrules/itranslate.js
@@ -1,7 +1,7 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<h1>502 Bad Gateway</h1>
-<p>The proxy server received an invalid response from an upstream server.<hr/>Powered by Tengine</body>
-</html>
+var obj = JSON.parse($response.body);
+
+obj= {"licenses": [{"is_trial_period": true, "product_id": "com.itranslate.pro.yearly", "original_transaction_id": "20000627379146", "expires_date_ms": 4099246456000, "bundle_id": "com.outerspaceapps.itranslate", "transaction_id": "20000627379146"}, {"is_trial_period": true, "product_id": "com.itranslate.pro.yearly", "original_transaction_id": "20000627379146", "expires_date_ms": 4099246456000, "bundle_id": "com.outerspaceapps.itranslate", "transaction_id": "20000627379146"}]};
+
+$done({body: JSON.stringify(obj)});
+
+// Descriptions
