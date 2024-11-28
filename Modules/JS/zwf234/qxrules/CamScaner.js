80acf219387055375ddf9ed41a8190648b94ce58
ifflagged <ifflagged@users.noreply.github.com>
2024-11-28 05:20:34 +0000

    Converted modules at 2024-11-28 13:20:33 (UTC+8)

--- Modules/JS/zwf234/qxrules/CamScaner.js
+++ Modules/JS/zwf234/qxrules/CamScaner.js
@@ -1,7 +1,3 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<h1>502 Bad Gateway</h1>
-<p>The proxy server received an invalid response from an upstream server.<hr/>Powered by Tengine</body>
-</html>
+let obj = JSON.parse($response.body);
+obj = {"data":{"psnl_vip_property":{"expiry":"4073275762"}}};
+$done({body: JSON.stringify(obj)});
