b44606339788ed4bcc783b9652517fcaed69a548
ifflagged <ifflagged@users.noreply.github.com>
2024-11-29 01:25:27 +0000

    Converted modules at 2024-11-29 09:25:27 (UTC+8)

--- Modules/JS/zwf234/qxrules/kinemaster.js
+++ Modules/JS/zwf234/qxrules/kinemaster.js
@@ -1,7 +1,14 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<h1>502 Bad Gateway</h1>
-<p>The proxy server received an invalid response from an upstream server.<hr/>Powered by Tengine</body>
-</html>
+var obj = JSON.parse($response.body);
+
+obj= {
+  "is_valid_device" : true,
+  "has_valid_subscription" : true,
+  "expiration_date_ms" : 4097755192000,
+  "is_table_resettable" : true,
+  "subscription_product_id" : "com.kinemaster.sub.annual.ia2",
+  "state_code" : 0
+};
+
+$done({body: JSON.stringify(obj)});
+
+// Descriptions
