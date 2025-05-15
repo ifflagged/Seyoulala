82ef5e11775dc55d7e3438117f6b15a02e199502
ifflagged <ifflagged@users.noreply.github.com>
2025-05-15 10:23:08 +0000

    Converted modules at 2025-05-15 18:23:07 (UTC+8)

--- Modules/JS/ddgksf2013/RevenueCat.Vip/revenuecat.js
+++ Modules/JS/ddgksf2013/RevenueCat.Vip/revenuecat.js
@@ -7,7 +7,7 @@
 > TgChannel         https://t.me/ddgksf2021
 > Contribute        https://t.me/ddgksf2013_bot
 > Feedback          📮 𝐝𝐝𝐠𝐤𝐬𝐟𝟐𝟎𝟏𝟑@𝟏𝟔𝟑.𝐜𝐨𝐦 📮
-> UpdateTime        2024-07-06
+> UpdateTime        2024-07-07
 > Suitable          自行观看“# > ”注释内容，解锁是暂时的，购买也不是永久的[订阅、跑路]
 > Attention         📣个别失效的APP请相关需求者自行降级、或寻找替代品、或购买支持
 > Attention         如需引用请注明出处，谢谢合作！
@@ -53,6 +53,7 @@ const mapping = {
   'StepUp/': ['premiun'],
   'SleepMaster/': ['premium','sm_14999_lifetime'],
   'Notedrafts': ['pro_entitlement'],
+  'Photon/': ['photon.paid','photon.bundle.yearly'],
   'Rats%20Project': ['PandaTracker_Premiumv2','monthly_subscription_discount_idv3'],
   'Grain/': ['gold','lifetimeMembership'],
   'AudioPlayer': ['Pro'],
