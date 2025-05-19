a7bf990959a3ee0618163edf82498c7aceb80659
ifflagged <ifflagged@users.noreply.github.com>
2025-05-19 23:20:39 +0000

    Converted modules at 2025-05-20 07:20:39 (UTC+8)

--- Modules/JS/Yuheng0101/heiliao/web.js
+++
@@ -1,8 +0,0 @@
-let body = $response.body;
-
-if (/<\/head>/.test(body)) {
-  body = body.replace(/<\/head>/, "</head><style>.editormd-preview blockquote,.footer-desc,.addbox,#notice_container,.gotoclick,.list-sec{display:none !important;}</style>");
-  $done({ body });
-}
-
-$done({});
