9520a5e805d32eb0a06c343cd52d3ba40423cd82
ifflagged <ifflagged@users.noreply.github.com>
2025-05-22 07:22:58 +0000

    Converted modules at 2025-05-22 15:22:57 (UTC+8)

--- Modules/JS/zirawell/ccblife/ccblife.js
+++ Modules/JS/zirawell/ccblife/ccblife.js
@@ -15,11 +15,19 @@ let obj = JSON.parse(body);
 const moduleKeys = [
   "TAG_AD_INFO",                  // 精选-右下角悬浮广告
   "NOTICE_AD_INFO",               // 精选-中间文字推荐
-  "HPBANNER_AD_INFO_SECOND",      // 精选-精选推荐
+  "PREFERENCE_AD_INFO",           // 精选-本地优惠
+  "HPBANNER_AD_INFO_SECOND",      // 精选-精选推荐-横幅
+  "DAY_BEST_AD_FIRST",            // 精选-精选推荐-轮播1
+  "DAY_BEST_AD_SECOND",           // 精选-精选推荐-轮播2
+  "DAY_BEST_AD_THIRD",            // 精选-精选推荐-轮播3
+  "DAY_BEST_AD_FOURTH",           // 精选-精选推荐-轮播4
+
   "LIFE_TOP_ROTATION_INFO_V3",    // 生活-上方轮播图
   "EDITOR_RECOMMEND2_AD",         // 生活-小编推荐
   "LIFE_V3_SCENE_AGGREGATION",    // 生活-分期·好生活
+
   "THROUGH_COLUMN_INFO",          // 金融-中间轮播图
+
   "MEBCT_AD_INFO",                // 我的-底部横幅广告
   "MYSELF_ENTRANCE_AD",           // 我的-财富会员入口
   
@@ -32,8 +40,14 @@ const blockKeys = [
 
 ];
 
-if (containBlockKey(url)) {
+const flowKeys = [
+  "A3341A095",                    // 精选-种草推荐
+  "A3341A068"                     // 金融-热门资讯
+];
+
+if (containKey(url,blockKeys)) {
   $done({body: "", headers: "", status: "HTTP/1.1 204 No Content"});
+
 } else if (url.includes("A3341AB04")) {
   if (obj?.data?.ICON_SKIN_INFO) {
     delete obj.data.ICON_SKIN_INFO;
@@ -52,8 +66,8 @@ if (containBlockKey(url)) {
   if (obj?.data?.POP_AD_INFO) {
     delete obj.data.POP_AD_INFO;
   }
-// 金融-热门资讯
-} else if (url.includes("A3341A068")) {
+// 信息流去除
+} else if (containKey(url,flowKeys)) {
   if (obj?.data?.data?.recList && obj.data.data.recList.length > 0) {
     delete obj.data.data.recList;
   }
@@ -63,6 +77,6 @@ body = JSON.stringify(obj);
 $done({body});
 
 
-function containBlockKey(str) {
-  return blockKeys.some(key => str.includes(key));
+function containKey(url,keys) {
+  return keys.some(key => url.includes(key));
 }
