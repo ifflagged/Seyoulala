9b7b0f86b8a1a3f9b3ad3c99a63862e54a001928
ifflagged <ifflagged@users.noreply.github.com>
2025-06-14 13:46:33 +0000

    Converted modules at 2025-06-14 21:46:33 (UTC+8)

--- Modules/JS/ddgksf2013/RevenueCat.Vip/revenuecat.js
+++ Modules/JS/ddgksf2013/RevenueCat.Vip/revenuecat.js
@@ -7,7 +7,7 @@
 > TgChannel         https://t.me/ddgksf2021
 > Contribute        https://t.me/ddgksf2013_bot
 > Feedback          📮 𝐝𝐝𝐠𝐤𝐬𝐟𝟐𝟎𝟏𝟑@𝟏𝟔𝟑.𝐜𝐨𝐦 📮
-> UpdateTime        2024-07-07
+> UpdateTime        2024-07-08
 > Suitable          自行观看“# > ”注释内容，解锁是暂时的，购买也不是永久的[订阅、跑路]
 > Attention         📣个别失效的APP请相关需求者自行降级、或寻找替代品、或购买支持
 > Attention         如需引用请注明出处，谢谢合作！
@@ -81,6 +81,7 @@ const mapping = {
   'Medication%20List/': ['Premium'],
   'Pillow': ['premium'],
   'DecibelMeter/': ['Premium'],
+  'nbcamera/': ['patron','com.andyworks.camera.yearlyPatron'],
   '1Blocker': ['premium'],
   'VSCO': ['membership'],
   'UTC': ['Entitlement.Pro'],
