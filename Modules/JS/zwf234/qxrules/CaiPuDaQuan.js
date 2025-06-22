0aac7714650333cf6f66dd8b12e64f845357764c
ifflagged <ifflagged@users.noreply.github.com>
2025-06-22 15:14:51 +0000

    Converted modules at 2025-06-22 23:14:51 (UTC+8)

---
+++ Modules/JS/zwf234/qxrules/CaiPuDaQuan.js
@@ -0,0 +1,8 @@
+
+<html>
+<head>
+<title>Fastly error: unknown domain raw.githubusercontent.com</title>
+</head>
+<body>
+<p>Fastly error: unknown domain: raw.githubusercontent.com. Please check that this domain has been added to a service.</p>
+<p>Details: cache-chi-kigq8000078-CHI (185.199.108.133)</p></body></html>
---
+++ Modules/JS/zwf234/qxrules/Wechat.js
@@ -0,0 +1,1 @@
+Same machine same service in the most recent hop on cache-chi-kigq8000051-CHI: xaLkLhUQg8FnOZp4rypZfrvxE38EPvgR1ya6ositf0I!CHI!cache-chi-kigq8000051-CHI
