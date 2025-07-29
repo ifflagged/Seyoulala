2a1dce93cf8eb267624a1c22a7d1276ca3bd2901
ifflagged <ifflagged@users.noreply.github.com>
2025-07-29 16:22:24 +0000

    Converted modules at 2025-07-30 00:22:23 (UTC+8)

--- Modules/JS/fmz200/blockAds/QuDa.js
+++ Modules/JS/fmz200/blockAds/QuDa.js
@@ -1,13 +1,1 @@
-/**
- * @author fmz200
- * @function 去哒 首页开屏广告
- * @date 2023-11-27 22:50:13
- */
-var body = $response.body;
-var obj = JSON.parse(body);
-if (obj.data.length == 600) {
-    obj.data = "null";
-    $done({body: JSON.stringify(obj)});
-} else {
-    $done({});
-}
+429: Too Many Requests
