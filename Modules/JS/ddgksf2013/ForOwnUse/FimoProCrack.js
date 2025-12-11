6d0563684ad6e7c4451074ed4c78578823a7a356
ifflagged <ifflagged@users.noreply.github.com>
2025-12-11 18:45:06 +0000

    Converted modules at 2025-12-12 02:45:05 (UTC+8)

--- Modules/JS/ddgksf2013/ForOwnUse/FimoProCrack.js
+++ Modules/JS/ddgksf2013/ForOwnUse/FimoProCrack.js
@@ -1,25 +1,3 @@
-/***********************************
-
-> ScriptName        fimo解锁会员
-> Author            @ddgksf2013
-> WechatID          墨鱼手记
-> TgChannel         https://t.me/ddgksf2021
-> Contribute        https://t.me/ddgksf2013_bot
-> Feedback          📮 ddgksf2013@163.com 📮
-> UpdateTime        2023-04-13
-> ScriptURL         https://github.com/ddgksf2013/MoYu/raw/master/FimoProCrack.js
-
-[rewrite_local]
-
-^https?:\/\/server\.zbisq\.com\/fimo-user url script-response-body https://github.com/ddgksf2013/MoYu/raw/master/FimoProCrack.js
-
-[mitm] 
-
-hostname = server.zbisq.com
-
-***********************************/
-
-
-
-
-var obj=JSON.parse($response.body);obj.subscribe={valid:!0,forever:1,endTime:4092599349},$done({body:JSON.stringify(obj)});
+<html><body><h1>504 Gateway Time-out</h1>
+The server didn't respond in time.
+</body></html>
