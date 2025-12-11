d958c7606fafc0ca932ee933260223c5775d7ed7
ifflagged <ifflagged@users.noreply.github.com>
2025-12-11 22:25:46 +0000

    Converted modules at 2025-12-12 06:25:45 (UTC+8)

--- Modules/JS/zirawell/abc/abc.js
+++ Modules/JS/zirawell/abc/abc.js
@@ -1,27 +1,3 @@
-/********************************
-ABC Remove Ads - Version 1.0
-Please note that you may need to reinstall app for script to work.
-
-QuantumultX rewrite link:
-https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/Z/中国农业银行/rewrite/abc.conf
-
-********************************/
-
-const url = $request.url;
-const header = $request.headers;
-const headopt = header["Operation-Type"] || header["operation-type"];
-
-const blockList = [
-    "com.bankabc.recommendcenter.homepage.gethpadverinfo",
-    "com.bankabc.credit.welfareCenter.getadverinfo",
-    //"com.bankabc.credit.home.getCcocAdInfo",
-    "com.bankabc.credit.query.custbillqry.getadv",
-    "com.abchina.mbank.common.homepage.getStartParam",
-    "alipay.client.updateVersion"
-];
-
-if (blockList?.includes(headopt)) {
-    $done({status: "HTTP/1.1 404 Not Found"});
-} else {
-    $done({});
-}
+<html><body><h1>504 Gateway Time-out</h1>
+The server didn't respond in time.
+</body></html>
