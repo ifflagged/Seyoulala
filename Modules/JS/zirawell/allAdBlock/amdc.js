638db5de5b36d5c94ee030fd7299c87714b1133a
ifflagged <ifflagged@users.noreply.github.com>
2026-03-05 08:46:21 +0000

    Converted modules at 2026-03-05 16:46:20 (UTC+8)

--- Modules/JS/zirawell/allAdBlock/amdc.js
+++ Modules/JS/zirawell/allAdBlock/amdc.js
@@ -1,17 +1,1 @@
-
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-  <head>
-    <title>503 between bytes timeout</title>
-  </head>
-  <body>
-    <h1>Error 503 between bytes timeout</h1>
-    <p>between bytes timeout</p>
-    <h3>Error 54113</h3>
-    <p>Details: cache-dfw-ktki8620085-DFW 1769727414 1852386690</p>
-    <hr>
-    <p>Varnish cache server</p>
-  </body>
-</html>
+upstream connect error or disconnect/reset before headers. retried and the latest reset reason: remote connection failure, transport failure reason: delayed connect error: Connection refused
