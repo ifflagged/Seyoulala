3580b733f762e75c59c567268c924bf7c7f82819
ifflagged <ifflagged@users.noreply.github.com>
2026-03-28 23:27:29 +0000

    Converted modules at 2026-03-29 07:27:29 (UTC+8)

--- Modules/JS/zwf234/qxrules/bimi.js
+++ Modules/JS/zwf234/qxrules/bimi.js
@@ -1,1 +1,2 @@
-upstream connect error or disconnect/reset before headers. retried and the latest reset reason: remote connection failure, transport failure reason: delayed connect error: Connection refused
+429: Too Many Requests
+For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
