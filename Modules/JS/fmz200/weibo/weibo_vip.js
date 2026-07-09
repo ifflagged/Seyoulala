b0de87302d74c81750a8cf5982e70f9e5a46ea01
ifflagged <ifflagged@users.noreply.github.com>
2026-07-09 07:16:15 +0000

    Converted modules at 2026-07-09 15:16:15 (UTC+8)

--- Modules/JS/fmz200/weibo/weibo_vip.js
+++ Modules/JS/fmz200/weibo/weibo_vip.js
@@ -1,2 +1,23 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+/**
+ * @author fmz200
+ * @function 解锁微博会员图标
+ * @date 2024-06-13 09:20:00
+ * 
+ * [MITM]
+ * hostname = new.vip.weibo.cn
+ * 
+ * [rewrite_local]
+ * ^https?://new\.vip\.weibo\.cn/aj/appicon/list url script-response-body https://github.com/fmz200/wool_scripts/raw/main/Scripts/weibo/weibo_vip.js
+ */
+
+let body = $response.body;
+let obj = JSON.parse(body);
+
+if (obj.data?.list) {
+  obj.data.list.forEach(function (item) {
+    item.cardType = "2";
+    item.tag = "";
+  });
+}
+
+$done({body: JSON.stringify(obj)});
