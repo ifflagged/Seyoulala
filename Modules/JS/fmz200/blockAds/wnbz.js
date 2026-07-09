eda816e15797cf6a44821c16d3a9dbccd088b051
ifflagged <ifflagged@users.noreply.github.com>
2026-07-09 00:30:47 +0000

    Converted modules at 2026-07-09 08:30:47 (UTC+8)

--- Modules/JS/fmz200/blockAds/wnbz.js
+++ Modules/JS/fmz200/blockAds/wnbz.js
@@ -1,2 +1,19 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+/*
+温尼伯站去广告 by Sliverkiss
+
+^https:\/\/vue3-api\.zhixiny\.cn\/v1\/initui\?key=index url script-response-body https://raw.githubusercontent.com/Sliverkiss/QuantumultX/main/AdBlock/xmApp/wnbz.js
+
+hostname = vue3-api.zhixiny.cn
+*/
+
+let body = JSON.parse($response.body);
+
+body.data.data = body.data.data.filter(e => e.key !== 'index_n');
+
+let indexItem = body.data.data.find(e => e.key === 'index');
+if (indexItem) {
+    let moduleList = indexItem.value.index.moduleList;
+    indexItem.value.index.moduleList = [moduleList[0], moduleList[moduleList.length - 1]];
+}
+
+$done(JSON.stringify(body));
