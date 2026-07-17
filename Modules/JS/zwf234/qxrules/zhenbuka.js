d686dde47594bdcd2b17b525cb998297a8d10ae2
ifflagged <ifflagged@users.noreply.github.com>
2026-07-17 05:02:23 +0000

    Converted modules at 2026-07-17 13:02:22 (UTC+8)

--- Modules/JS/zwf234/qxrules/zhenbuka.js
+++ Modules/JS/zwf234/qxrules/zhenbuka.js
@@ -1,1 +1,17 @@
-upstream connect error or disconnect/reset before headers. retried and the latest reset reason: remote connection failure, transport failure reason: delayed connect error: Connection refused
+
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title>503 first byte timeout</title>
+  </head>
+  <body>
+    <h1>Error 503 first byte timeout</h1>
+    <p>first byte timeout</p>
+    <h3>Error 54113</h3>
+    <p>Details: cache-iad-kiad7000119-IAD 1784264488 3717020101</p>
+    <hr>
+    <p>Varnish cache server</p>
+  </body>
+</html>
