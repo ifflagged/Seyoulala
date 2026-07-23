87fb310cf92f6387c3f078ec084c54cd774e37cb
ifflagged <ifflagged@users.noreply.github.com>
2026-07-23 23:59:02 +0000

    Converted modules at 2026-07-24 07:59:01 (UTC+8)

--- Modules/JS/fmz200/rewrite/QuDa.js
+++ Modules/JS/fmz200/rewrite/QuDa.js
@@ -1,13 +1,17 @@
-/**
- * @author fmz200
- * @function 去哒 首页开屏广告
- * @date 2023-11-27 22:50:13
- */
-var body = $response.body;
-var obj = JSON.parse(body);
-if (obj.data.length == 600) {
-    obj.data = "null";
-    $done({body: JSON.stringify(obj)});
-} else {
-    $done({});
-}
+
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title>503 first byte timeout</title>
+  </head>
+  <body>
+    <h1>Error 503 first byte timeout</h1>
+    <p>first byte timeout</p>
+    <h3>Error 54113</h3>
+    <p>Details: cache-iad-kjyo7100114-IAD 1784850969 191318421</p>
+    <hr>
+    <p>Varnish cache server</p>
+  </body>
+</html>
